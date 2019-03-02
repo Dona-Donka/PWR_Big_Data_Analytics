33294c0d832d53eede483ab3b17811fbb147eac7
Dominika Maślanka <36755559+Dona-Donka@users.noreply.github.com>
2019-03-02 11:40:50 +0100

    List01 (programming and classification) created /task8!

---
+++ list01_functions.py
@@ -0,0 +1,32 @@
+import random
+
+class Functions():
+    def random_function(len):
+        list = []
+        x = 0
+        while x < len:
+            list.append(random.randint(1, 90))
+            x += 1
+        return list
+
+
+    def odd_even_check(x):
+        if (x % 2 == 0):
+            return print(x, "is a odd number")
+        else:
+            return print(x, "is an even number")
+
+
+    def is_digit(sentence, digit, letter):
+
+        if sentence.isdigit() == True:
+            print("This is not a sentence! This is a digit")
+        elif sentence.isalpha() == True:
+            print("This is not a sentence! This is an alpha")
+        else:
+            for item in sentence:
+                if item.isdigit() == True:
+                    digit += 1
+                elif item.isalpha() == True:
+                    letter += 1
+        return print("sentence: ", sentence, "\n", "number of letter: ", letter, "\n", "number of digit: ", digit, "\n" )
