c5d4a428c44ab4f1b41c70c9e976f4e604a04009
Dominika Maślanka <36755559+Dona-Donka@users.noreply.github.com>
2019-03-03 09:55:49 +0100

    The whole List01.scala has been made

--- List01.scala
+++ List01.scala
@@ -1,17 +1,19 @@
 
 package SCALA_programming_and_classification
 
-/* mport SCALA_programming_and_classification.List01.args */
+/* mport SCALA_programming_and_classification.List01.args /*
 
 
-/* ------ HelloScala---------\* 
+/* ------ HelloScala---------/* 
+
 object HelloScala {
   def main(args: Array[String]): Unit = {
     println("Hello World!")
   }
 }
 
-/*------ScalaParameters---------*\
+/*------ScalaParameters---------/*
+
 object ScalaParameters {
   def main(parameters: Array[String]): Unit = {
     if (parameters.length == 0)
@@ -22,3 +24,29 @@ object ScalaParameters {
   }
 }
 
+/*------TheGreatestDivisior----------/*
+
+import scala.collection.mutable.ListBuffer
+import scala.util.control.Breaks._
+
+object TheGreatestDivisor{
+   def main(parameters: Array[String]): Unit = {
+     var sum: Int = 0
+     var divList = new ListBuffer[Int]()
+     val numbers_list: List[Int] = List(24, 13, 50, 46, 9)
+     for (number <- numbers_list) {
+       for (div <- 1 to number) {
+         divList += div
+       }
+       divList -= number
+       breakable {for (item <- divList.reverseIterator){
+         if (number % item == 0){
+           println(number + " : " + item)
+           break()
+         }}
+       }
+       divList.clear()
+
+     }
+  }
+}
