24a622486c771635a153a4917dd0cd80c60d8dd9
Dominika Maślanka <36755559+Dona-Donka@users.noreply.github.com>
2019-03-03 08:57:26 +0100

    2/3 tasks of List01.scala done

---
+++ List01.scala
@@ -0,0 +1,24 @@
+
+package SCALA_programming_and_classification
+
+/* mport SCALA_programming_and_classification.List01.args */
+
+
+/* ------ HelloScala---------\* 
+object HelloScala {
+  def main(args: Array[String]): Unit = {
+    println("Hello World!")
+  }
+}
+
+/*------ScalaParameters---------*\
+object ScalaParameters {
+  def main(parameters: Array[String]): Unit = {
+    if (parameters.length == 0)
+    println("Set an argument!")
+    else{
+      parameters.foreach(println(_))
+    }
+  }
+}
+
