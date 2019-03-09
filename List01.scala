9b7717f95fa6ee33f1150760d460760f6dbff944
Dominika Maślanka <36755559+Dona-Donka@users.noreply.github.com>
2019-03-09 12:21:06 +0100

    task3 "exception" added (ver.1.1)

--- List01.scala
+++ List01.scala
@@ -50,3 +50,46 @@ object TheGreatestDivisor{
      }
   }
 }
+
+
+/*------TheGreatestDivisior_ver.1.1----------/*
+
+import scala.collection.mutable.ListBuffer
+import scala.util.control.Breaks._
+
+object TheGreatestDivisor_ver2 {
+  def main(parameters: Array[String]): Unit = {
+
+
+    var number_list = new ListBuffer[Int]()
+    var divList = new ListBuffer[Int]()
+
+    if (parameters.length == 0)
+      println("Set an argument!")
+
+    parameters.foreach(number =>
+      try {
+       number_list += number.toInt
+      }
+      catch {
+        case exception: NumberFormatException =>
+          println(number ,"Conversion not possible")
+      })
+
+      println(number_list)
+    for (number <- number_list) {
+      for (div <- 1 to number) {
+        divList += div
+      }
+
+      divList -= number
+      breakable {for (item <- divList.reverseIterator){
+        if (number % item == 0){
+          println(number + " : " + item)
+          break()
+        }}
+      }
+      divList.clear()
+    }
+  }
+}
