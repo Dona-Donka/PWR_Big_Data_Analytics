e356488fd051f628ea774d88c9ed60a14b43fe82
Dominika Maślanka <36755559+Dona-Donka@users.noreply.github.com>
2019-03-24 18:26:10 +0100

    tasks 7-9 done (9/10)

--- List02.py
+++ List02.py
@@ -44,9 +44,66 @@ def task06(usersDataDict):
     return sorted(usersDataDict.items(), key=operator.itemgetter(1))
 
 def task07():
-    nltk.download('gutenberg')
+    a = text6.count("knight")
+    b = text7.count("knight")
+    return (" 'knight' in text6: ", a,  "\n",
+            " 'knight' in text7: ", b,)
 
-    return 0
+def task8():
+    listOftext6 = [x for x in text6]
+    listOftext7 = [x for x in text7]
+    c = [x for x in  listOftext6 if x not in listOftext7]
+    return c
+
+def task9():
+    t1 = set(text1)
+    t2 = set(text2)
+    t3 = set(text3)
+    t4 = set(text4)
+    t5 = set(text5)
+    t6 = set(text6)
+    t7 = set(text7)
+    t8 = set(text8)
+    t9 = set(text9)
+    allSets = t1 & t2 & t3 & t4 & t5 & t6 & t7 & t8 & t9
+    return allSets
+
+# - do poprawy -
+def task10():
+    t = list(text2)
+    print(t)
+    helpList = []
+    counter = 0
+    largestNumber = 0
+    theLargestSentence = []
+    for word in t:
+        if word != '.':
+            helpList.append(word)
+            counter += 1
+        else:
+            if counter > largestNumber:
+                largestNumber = counter
+                theLargestSentence = helpList
+                helpList = []
+                counter = 0
+
+
+    return "contain: ", largestNumber, "words",
+
+
+################################
+s = set([1, 2, 3 ,4, 5, 6])
+a = set([2,4,7])
+
+# union = {1, 2, 3, 4, 5, 6, 7}
+print( a | s)
+
+# intersection = {2, 4}
+print( a & s)
+
+# difference = {7}
+print( a - s)
+################################
 
 
 #print(task01(-5,10))
@@ -60,4 +117,7 @@ mixedList = [15, "Ali", -3, "Mathew", 44, 54, "Walerian"]
 #print(task05(mixedList))
 usersDataDict= {"Marek":"Radom", "Phillip":"Cork", "Ludwik":"Aspen", "Lea":"Berlin"}
 #print(task06(usersDataDict))
-print(task07())
+#print(task07())
+#print(task8())
+#print(task9())
+print(task10())
