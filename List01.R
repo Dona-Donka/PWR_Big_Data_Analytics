a4b9a40ac7c2f20265ec22166429aad82627e727
Dominika Maślanka <36755559+Dona-Donka@users.noreply.github.com>
2019-03-25 22:05:05 +0100

    task 3 added 2/3

--- List01.R
+++ List01.R
@@ -1,3 +1,7 @@
+# list 01 task 2/4
+# Explain Gaussian method of computing an inverse of a matrix. 
+# Implement this method
+
 print("matrix: ")
 #M <- matrix(c(0, 2, 3, 7), 2, 2)
 M <- matrix(c(2,6,5,5,3,-2,7,4,-3), 3,3)
@@ -10,8 +14,7 @@ for (col in 1:ncol(M))
     if (A[col,col] == 0) {
         for (r in 2:nrow(A)){
             A[c(col,r),] <- A[c(r,col),]
-        }
-        print("after if")        
+        }       
     }
     
     A[col,] <- A[col,] / A[col,col]  
@@ -22,3 +25,26 @@ for (col in 1:ncol(M))
     }
 print("Result: ")
 A
+
+# list 01 task 3/4
+# Explain Gaussian method of computing the rank of a matrix.
+# Implement this method.
+
+#M <- matrix(c(0, 2, 3, 7), 2, 2)
+M <- matrix(c(2,6,5,5,3,-2,7,4,-3), 3,3)
+M
+for (col in 1:ncol(M))
+{
+    if (M[col,col] == 0) {
+        for (r in 2:nrow(M)){
+            M[c(col,r),] <- M[c(r,col),]
+        }
+    }
+    
+    M[col,] <- M[col,] / M[col,col]  
+    
+    for (row in 1:nrow(M)){
+      if (row > col){ M[row,] <- M[row,] - M[row,col]*M[col,]}
+        }
+    }
+M
