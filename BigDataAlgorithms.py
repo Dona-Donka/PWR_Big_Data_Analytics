3bdb3d74c4b9f43ecf99d201187339e717b1de73
Dominika Maślanka <36755559+Dona-Donka@users.noreply.github.com>
2019-10-21 15:29:40 +0200

    TF-IDF added

--- BigDataAlgorithms.py
+++ BigDataAlgorithms.py
@@ -10,3 +10,11 @@ for key, value in sorted(list.items(), key=lambda item: item[1], reverse=True):
     dictFile.write("%s %s \n" % (key, value))
 
 ##########################################################################################
+# TD-IDF
+sum_of_words = 0
+for key, value in word_list.items():
+    sum_of_words += value
+
+print("sum: ", sum_of_words)
+for key, value in sorted(word_list.items(), key=lambda item: item[1], reverse=True):
+    print("word: ", key, "TD-IDF: ", value, "/", sum_of_words, "=", value/sum_of_words)
