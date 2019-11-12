9349437b7363fe81de82a861aa290a82b7a4a1e1
Dominika Maślanka <36755559+Dona-Donka@users.noreply.github.com>
2019-11-12 19:38:22 +0100

    Update wattsStrogatzGraph.py

--- wattsStrogatzGraph.py
+++ wattsStrogatzGraph.py
@@ -56,3 +56,52 @@ for node in range(0,N):
     print("node: ", node, " Shortest paths: ", pathLength[1][node])
 print("Average paths: ", statistics.mean([int(v) for v in pathLength[1].values()]))
 print("Diameter: ", nx.diameter(ws))
+
+############################################################################################
+
+import networkx as nx
+import matplotlib.pyplot as plt
+
+averagePathList = []
+averageClusteringCoeffc = []
+dataList = [0.0001, 0.0005, 0.001, 0.0025, 0.005, 0.010, 0.025, 0.050, 0.1, 0.15, 0.25, 0.35, 0.5, 0.65, 0.85, 1]
+for data in dataList:
+    ws1 = nx.watts_strogatz_graph(1000, 8, data)
+    averagePathList.append(nx.average_shortest_path_length(ws1))
+    averageClusteringCoeffc.append(nx.average_clustering(ws1))
+
+averagePathList1 = []
+for item in averagePathList:
+    averagePathList1.append(item/averagePathList[0])
+
+averageClusteringCoeffc1 = []
+for item in averageClusteringCoeffc:
+    averageClusteringCoeffc1.append(item/averageClusteringCoeffc[0])
+
+#p1 = plt.scatter(dataList, averageClusteringCoeffc1, color='seagreen')
+#p2 = plt.scatter(dataList, averagePathList1, color='brown')
+
+#plt.title("Increasing randomness")
+#plt.xlabel("p")
+#plt.legend((p1,p2),("<C[p]>/<C[0]>", "d[p]/d[0]"))
+#plt.savefig(PATH)
+
+import numpy as np
+
+N = [5, 10, 20, 30, 50, 75, 100, 150, 200, 250, 500, 750, 1000, 1250]
+averageDiameters = []
+averagePaths = []
+theoreticalResults = []
+
+for data in N:
+    ws2 = nx.watts_strogatz_graph(data, 4, 0.25)
+    averagePaths.append(nx.average_shortest_path_length(ws2))
+    averageDiameters.append(nx.diameter(ws2))
+    theoreticalResults.append(np.log(data)/np.log(4))
+
+p3 = plt.scatter(N, averagePaths, color='darkslategray')
+p4 = plt.scatter(N, averageDiameters, color='midnightblue')
+p5 = plt.scatter(N, theoreticalResults, color='black')
+plt.xlabel("N")
+plt.legend((p3,p4, p5),("Paths", "Diameters", "theoretical"))
+plt.savefig(PATH)
