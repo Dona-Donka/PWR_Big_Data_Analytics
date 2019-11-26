d905e11f5b1d3a4669f91a96e18949dae82bb433
Dominika Maślanka <36755559+Dona-Donka@users.noreply.github.com>
2019-11-26 14:19:05 +0100

    closeness and betweenness centrality added

--- networkRobustness.py
+++ networkRobustness.py
@@ -1,37 +1,75 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import matplotlib.legend as ax
 import random
-
+import numpy as np
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning)
 
 size = 3
-def computeComponent(graph, size):
-    components = []
-    components1 = []
-    graph1 = graph.copy()
-    list = [x[0] for x in sorted(dict(graph.degree()).items(), reverse=True, key=lambda x: x[1])]
 
+def connectedComponentsList(graph, nodeList):
+    list = []
     for r in range(1,100):
-        graph.remove_nodes_from(list[:int((10**size)*0.01)])
-        list = list[int((10**size)*0.01):]
-        components1.append(len(max(nx.connected_components(graph))))
+        graph.remove_nodes_from(nodeList[:int((10 ** size) * 0.01)])
+        nodeList = nodeList[int((10 ** size) * 0.01):]
+        list.append(len(max(nx.connected_components(graph))))
+    return list
 
-        graph1.remove_nodes_from(random.sample(graph1.nodes(), int((10**size)*0.01)))
-        components.append(len(max(nx.connected_components(graph1))))
+def computeMean(list):
+    return np.mean(list, axis=0)
 
+def computeComponent(size):
+    randomCompMean = []
+    degreeCompMean = []
+    closenessCompMean = []
+    betweennessCompMean = []
+
+    for r in range(1,250):
+        graph = nx.watts_strogatz_graph(10 ** size, 4, 0.01)
+        graph1 = graph.copy()
+        graph2 = graph.copy()
+        graph3 = graph.copy()
+
+        # Removing high-degree nodes,  creating a list of conneted_components lenght (attack)
+        listDegree = [x[0] for x in sorted(dict(graph.degree()).items(), reverse=True, key=lambda x: x[1])]
+        degreeCompMean.append(connectedComponentsList(graph, listDegree))
+        
+        # Removing random sample nodes, creating a list of connected_components lenght
+        listRandomNodes = random.sample(graph1.nodes(), len(graph1.nodes()))
+        randomCompMean.append(connectedComponentsList(graph1, listRandomNodes))
+
+        # Removing of high closeness_centrality nodes, creating a list of connected_components lenght
+        listClosenessCentrality = [x[0] for x in sorted(dict(nx.closeness_centrality(graph)).items(), reverse=True, key=lambda x: x[1])]
+        closenessCompMean.append(connectedComponentsList(graph2, listClosenessCentrality))
+
+        # Removing of high betweeness_centrality nodes, creating a list of connected_components lenght
+        listBetweennessCentrality = [x[0] for x in sorted(dict(nx.betweenness_centrality(graph)).items(), reverse=True, key=lambda x: x[1])]
+        betweennessCompMean.append(connectedComponentsList(graph3, listBetweennessCentrality))
+
+    degreeCompMean = computeMean(degreeCompMean)
+    randomCompMean = computeMean(randomCompMean)
+    closenessCompMean = computeMean(closenessCompMean)
+    betweennessCompMean = computeMean(betweennessCompMean)
+
+    # plotting:
     x = [x*0.01 for x in range(1,100)]
-    plt.plot(x, [x/10**size for x in components1], 'b-',
-             x, [x/10**size for x in components], 'r-')
+    degree, = plt.plot(x, [x/10**size for x in degreeCompMean], label = "degree (attack)")
+    randoms, = plt.plot(x, [x/10**size for x in randomCompMean], label = "random")
+    closeness, =   plt.plot(x, [x/10**size for x in closenessCompMean], label = "closeness")
+    betweeness = plt.plot(x, [x/10**size for x in betweennessCompMean], label = "betweeness")
 
-    plt.title("Robustness of networks")
+    # drawing legend and titles:
+    legend = plt.legend(bbox_to_anchor=(0.96, 0.94), loc = "upper right", borderaxespad=0.)
+    plt.gca().add_artist(legend)
+    plt.title("Robustness of networks" + "\n" + "Watts-Strogatz, N = 1000, k = 4, p = 0.01")
     plt.xlabel("Removed nodes")
     plt.ylabel("connected components coefficient")
-    plt.savefig(PATH)
+    plt.savefig("/home/dona/Pulpit/complexy_3.jpg")
 
 
 ws = nx.watts_strogatz_graph(10 ** size, 4, 0.01)
 ba = nx.barabasi_albert_graph(10**size, 2)
 er = nx.erdos_renyi_graph(10**size, 2)
 
-computeComponent(ws,3)
+computeComponent(3)
