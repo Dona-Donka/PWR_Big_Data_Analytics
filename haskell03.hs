bab8ad08e80b686e8c76ebe39f816b7a9e6437f8
Dominika Maślanka <36755559+Dona-Donka@users.noreply.github.com>
2019-12-06 10:27:45 +0100

    Vector3D added

--- haskell03.hs
+++ haskell03.hs
@@ -13,3 +13,21 @@ surface (Rectangle (Point x1 y1) (Point x2 y2)) = (abs $ x2 - x1) * (abs $ y2 - 
 
 -- main = print $ (surface (Circle (Point 10.0 10.0) 5))
 -- main = print $ (surface (Rectangle (Point 2.0 3.0) (Point 7.0 8.0)))
+
+-----------------------------------------------------------------------------------------------
+
+-- Exercise 02, Let us define: data Vector3D a = Vector a a a
+-- that models 3D vectors. Define addition, multiplication by a scalar and scalar multiplica-tion for your vectors
+
+data Vector3D a = Vector3D a a a deriving(Show)
+
+-- addicion <u1,u2> + <v1,v2> = <u1+v1, u2+v2>
+addicion :: (Num a) =>  Vector3D a -> Vector3D a -> Vector3D a  
+(Vector3D u1 u2 u3) `addicion`  (Vector3D v1 v2 v3)  = Vector3D (u1+v1) (u2+v2) (u3+v3)
+
+-- multiplication by a scalar s  s<u1,u2> = <u1*s, u2*s>
+multByScalar :: (Num a) => Vector3D a -> a -> Vector3D a
+(Vector3D u1 u2 u3) `multByScalar` s = Vector3D (u1*s) (u2*s) (u3*s)
+
+-- main = print $ ((Vector3D 1 1 1) `addicion`  (Vector3D 2 2 2) )
+-- main = print $ ((Vector3D 1 1 1) `multByScalar` 4)
