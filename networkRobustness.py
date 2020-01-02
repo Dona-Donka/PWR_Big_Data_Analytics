cbac09cade3819257232e27448cf4ea20def4d6e
Dominika Maślanka <36755559+Dona-Donka@users.noreply.github.com>
2020-01-02 14:05:31 +0100

    robustness fixed

--- networkRobustness.py
+++ networkRobustness.py
@@ -6,14 +6,18 @@ import numpy as np
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning)
 
-size = 3
 
-def connectedComponentsList(graph, nodeList):
+def connectedComponentsList(graph, nodeList, size):
     list = []
+    giant = sorted(nx.connected_components(graph), key=len, reverse=True)
+    g0 = (graph.subgraph(giant[0])).size()
+    
     for r in range(1,100):
         graph.remove_nodes_from(nodeList[:int((10 ** size) * 0.01)])
         nodeList = nodeList[int((10 ** size) * 0.01):]
-        list.append(len(max(nx.connected_components(graph))))
+        giant1 = sorted(nx.connected_components(graph), key=len, reverse=True)
+        g1_0 = (graph.subgraph(giant1[0])).size()
+        list.append(g1_0/g0)
     return list
 
 def computeMean(list):
@@ -25,51 +29,51 @@ def computeComponent(size):
     closenessCompMean = []
     betweennessCompMean = []
 
-    for r in range(1,250):
-        graph = nx.watts_strogatz_graph(10 ** size, 4, 0.01)
+    for r in range(1,5):
+        print("step: ", r)
+        graph = nx.random_geometric_graph(10 ** size, 0.5)
         graph1 = graph.copy()
         graph2 = graph.copy()
         graph3 = graph.copy()
 
         # Remove high-degree nodes,  create a list of conneted_components lenght (attack)
         listDegree = [x[0] for x in sorted(dict(graph.degree()).items(), reverse=True, key=lambda x: x[1])]
-        degreeCompMean.append(connectedComponentsList(graph, listDegree))
-        
+        degreeCompMean.append(connectedComponentsList(graph, listDegree,size))
+
         # Remove random sample nodes, create a list of connected_components lenght
         listRandomNodes = random.sample(graph1.nodes(), len(graph1.nodes()))
-        randomCompMean.append(connectedComponentsList(graph1, listRandomNodes))
+        randomCompMean.append(connectedComponentsList(graph1, listRandomNodes,size))
 
         # Remove high closeness_centrality nodes, create a list of connected_components lenght
-        listClosenessCentrality = [x[0] for x in sorted(dict(nx.closeness_centrality(graph)).items(), reverse=True, key=lambda x: x[1])]
-        closenessCompMean.append(connectedComponentsList(graph2, listClosenessCentrality))
+        listClosenessCentrality = [x[0] for x in sorted(dict(nx.closeness_centrality(graph2)).items(), reverse=True, key=lambda x: x[1])]
+        closenessCompMean.append(connectedComponentsList(graph2, listClosenessCentrality,size))
 
-        # Remove high betweeness_centrality nodes, create a list of connected_components lenght
-        listBetweennessCentrality = [x[0] for x in sorted(dict(nx.betweenness_centrality(graph)).items(), reverse=True, key=lambda x: x[1])]
-        betweennessCompMean.append(connectedComponentsList(graph3, listBetweennessCentrality))
+       # Remove high betweeness_centrality nodes, create a list of connected_components lenght
+        listBetweennessCentrality = [x[0] for x in sorted(dict(nx.betweenness_centrality(graph3)).items(), reverse=True, key=lambda x: x[1])]
+        betweennessCompMean.append(connectedComponentsList(graph3, listBetweennessCentrality,size))
 
     degreeCompMean = computeMean(degreeCompMean)
     randomCompMean = computeMean(randomCompMean)
     closenessCompMean = computeMean(closenessCompMean)
     betweennessCompMean = computeMean(betweennessCompMean)
 
-    # plotting:
+    # plot:
     x = [x*0.01 for x in range(1,100)]
-    degree, = plt.plot(x, [x/10**size for x in degreeCompMean], label = "degree (attack)")
-    randoms, = plt.plot(x, [x/10**size for x in randomCompMean], label = "random")
-    closeness, =   plt.plot(x, [x/10**size for x in closenessCompMean], label = "closeness")
-    betweeness = plt.plot(x, [x/10**size for x in betweennessCompMean], label = "betweeness")
+    print("x: ", x)
+    degree, = plt.plot(x, degreeCompMean, label = "degree (attack)")
+    randoms, = plt.plot(x, randomCompMean, label = "random")
+    closeness, =   plt.plot(x, closenessCompMean, label = "closeness")
+    betweeness = plt.plot(x, betweennessCompMean, label = "betweeness")
 
     # drawing legend and titles:
     legend = plt.legend(bbox_to_anchor=(0.96, 0.94), loc = "upper right", borderaxespad=0.)
     plt.gca().add_artist(legend)
-    plt.title("Robustness of networks" + "\n" + "Watts-Strogatz, N = 1000, k = 4, p = 0.01")
+    plt.title("Robustness of networks" + "\n" + "Random Graph, N = 1000, k = 8")
     plt.xlabel("Removed nodes")
     plt.ylabel("connected components coefficient")
-    plt.savefig("/home/dona/Pulpit/complexy_3.jpg")
+    plt.savefig("random_N1000_k8.jpg")
 
 
-ws = nx.watts_strogatz_graph(10 ** size, 4, 0.01)
-ba = nx.barabasi_albert_graph(10**size, 2)
-er = nx.erdos_renyi_graph(10**size, 2)
+
 
 computeComponent(3)
